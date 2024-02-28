7077f68dc06ebe230fd03fb37e0fcca5fd33c595
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-02-28 12:11:44 -0600

    added test cases

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -1,6 +1,9 @@
 function factorial(num){
+  if(num < 1) return 1;
   if(num === 1 ) return 1;
   return num * factorial(num -1)
 }
 
-console.log(factorial(10) )
+console.log(factorial(10))
+console.log(factorial(5))
+console.log(factorial(-3))
