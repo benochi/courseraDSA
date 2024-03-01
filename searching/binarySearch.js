677b9a408289646a08f5ec3c5ef165e5065dfed0
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-03-01 12:49:37 -0600

    added non recursive binarySort

--- searching/binarySearch.js
+++ searching/binarySearch.js
@@ -14,8 +14,24 @@ function binarySearch(arr, val, left=0, right=arr.length-1){
   } else {
     return binarySearch(arr, val, middle + 1, right)
   }
-
 }
 
+function notRecursiveBinarySearch(arr, val){
+  let left = 0;
+  let right = arr.length -1;
+  let middle = Math.floor((left + right) / 2);
+
+  while(arr[middle] !== val && left <= right){
+    if(val < arr[middle]){
+      right = middle - 1;
+    } else {
+      left = middle + 1;
+    }
+    middle = Math.floor((left + right) / 2);
+  }
+  return arr[middle] == val ? middle : -1;
+}
 console.log(binarySearch(sortedIntArray, 40))
 console.log(binarySearch(sortedIntArray, 4000))
+console.log(notRecursiveBinarySearch(sortedIntArray, 40))
+console.log(notRecursiveBinarySearch(sortedIntArray, 4000))
