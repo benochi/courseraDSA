5f1ee083ba33a323180b59814cb870c785b8c5d6
Dan Politica <73758508+benochi@users.noreply.github.com>
2024-03-01 11:56:49 -0600

    made a linear search for object key by val

--- searching/linearSearch.js
+++ searching/linearSearch.js
@@ -1,5 +1,11 @@
 const { intArray, strArray } = require("./array.js");
 //linearSearch is used to check every element one at a time.  indexOf does this. 
+const newObj = {
+  1:'batman',
+  'tim':'The grey',
+  11.2:'float'
+}
+
 
 function findIndex(arr, val){
   for(let i =0; i < arr.length; i++){
@@ -10,7 +16,20 @@ function findIndex(arr, val){
   return -1
 }
 
+function findObjKeyByVal(obj, val){
+  for(let key in obj){
+    if(obj[key] === val){
+      return key;
+    }
+  }
+  return -1
+}
+
 console.log(findIndex(intArray, 12));
 console.log(findIndex(intArray, 42));
 console.log(findIndex(strArray, 'dan'));
 console.log(findIndex(strArray, 'Garbuldeeguck'));
+
+console.log(findObjKeyByVal(newObj, 'float'));
+console.log(findObjKeyByVal(newObj, 'batman'));
+console.log(findObjKeyByVal(newObj, 'burrito'));
